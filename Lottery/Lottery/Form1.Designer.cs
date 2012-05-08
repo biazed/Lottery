de8e7b1c7533f5c9968156dd8f2c9f99b0850a52
biazed <biazed.pwn@gmail.com>
2012-05-09 01:19:49 +0300

    Upload

---
+++ Lottery/Lottery/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Lottery
+{
+    partial class Lottery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Lottery));
+            this.go = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // go
+            // 
+            this.go.BackColor = System.Drawing.Color.Red;
+            this.go.BackgroundImage = global::Lottery.Properties.Resources.goBtn;
+            this.go.FlatAppearance.BorderSize = 0;
+            this.go.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.go.Location = new System.Drawing.Point(158, 90);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(143, 23);
+            this.go.TabIndex = 0;
+            this.go.UseVisualStyleBackColor = false;
+            this.go.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Tomato;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Tomato;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Tomato;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(150, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 33);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Tomato;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(215, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 33);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Tomato;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(276, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 33);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Tomato;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(343, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 33);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Tomato;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(408, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 33);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "0";
+            // 
+            // exit
+            // 
+            this.exit.BackgroundImage = global::Lottery.Properties.Resources.exitBtn;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exit.Location = new System.Drawing.Point(428, 2);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(23, 23);
+            this.exit.TabIndex = 8;
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Lottery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(459, 128);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.go);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Lottery";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lottery";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ Lottery/Lottery/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BiazedLib;
+
+namespace Lottery
+{
+
+    public partial class Lottery : Form
+    {
+
+        public Lottery()
+        {
+            InitializeComponent();
+            initialize();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> numbers = new List<int>();
+            BiazedLib.Sorting sort = new BiazedLib.Sorting();
+            BiazedLib.Math Math = new BiazedLib.Math();
+            numbers = sort.bubblesort(validate(Math.generate(7,1,39)));
+            label1.Text = numbers[0].ToString();
+            label2.Text = numbers[1].ToString();
+            label3.Text = numbers[2].ToString();
+            label4.Text = numbers[3].ToString();
+            label5.Text = numbers[4].ToString();
+            label6.Text = numbers[5].ToString();
+            label7.Text = numbers[6].ToString();
+
+
+        }
+
+
+        private List<int> validate(List<int> input)
+        {
+            Random numb = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (i != j)
+                    {
+                        if (input[i] == input[j])
+                        {
+                            input[i] = numb.Next(1,39);
+                            validate(input);
+                        }
+                    }
+                }
+                
+            }
+            return input;
+        }
+
+        private void initialize()
+        {
+                label1.Text = "0";
+                label2.Text = "0";
+                label3.Text = "0";
+                label4.Text = "0";
+                label5.Text = "0";
+                label6.Text = "0";
+                label7.Text = "0";
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
